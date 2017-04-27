928b5e831d119438fc99046c2a35bdc463680b3a
Keith Evans <chen.evans+github@gmail.com>
2017-04-27 13:07:39 -0700

    Preparing for byte write enables

--- src/rtl/flunkyfive.v
+++ src/rtl/flunkyfive.v
@@ -14,19 +14,47 @@ module flunkyfive (
     input wire [31:0]   pwdata
 );
 
-true_dual_port_ram_single_clock #(
-    .DATA_WIDTH(32),
-    .ADDR_WIDTH(14)
-) ram (
-    .clk                (clk),
-    .we_a               (pwrite & psel & penable),
-    .addr_a             (paddr[15:2]),
-    .data_a             (pwdata ^ 32'h5555_5555), // TBD - hack to make sure it REALLY works
-    .q_a                (prdata),
-    .we_b               ('b0),
-    .addr_b             ('b0),
-    .data_b             ('b0),
-    .q_b                ()
+    // -------------------------------
+    // PicoRV32 Core
+    wire [31:0]         mem_addr;
+    wire [31:0]         mem_wdata;
+    wire [3:0]          mem_wstrb;
+
+    reg                 mem_ready;
+    reg [31:0]          mem_rdata;
+
+    picorv32 #(
+        .ENABLE_REGS_DUALPORT(1),
+        .COMPRESSED_ISA(1),
+        .ENABLE_COUNTERS(0),
+        .LATCHED_MEM_RDATA(1),
+        .TWO_STAGE_SHIFT(0),
+        .TWO_CYCLE_ALU(1),
+        .CATCH_MISALIGN(0),
+        .CATCH_ILLINSN(0)
+    ) cpu (
+        .clk        (clk      ),
+        .resetn     (resetn   ),
+        .mem_valid  (mem_valid),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr ),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata)
+    );
+
+    shared_ram #(
+        .ADDR_WIDTH(14)
+    ) ram (
+        .clk        (clk),
+        .we_a       ({4{pwrite & psel & penable}}),
+        .addr_a     (paddr[15:2]),
+        .data_a     (pwdata ^ 32'h5555_5555), // TBD - hack to make sure it REALLY works
+        .q_a        (prdata),
+        .we_b       (4'b0),
+        .addr_b     (14'b0),
+        .data_b     (32'b0),
+        .q_b        ()
 );
 
 endmodule
